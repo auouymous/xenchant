6011459bc5293202506d45d6c63972681fc0bc5c
auouymous <au@qzx.com>
2020-03-12 02:32:34 -0600

    more enchant level/cost balancing

--- config-SAMPLE.lua
+++ config-SAMPLE.lua
@@ -2,6 +2,9 @@
 
 
 
+-- there needs to be multiple enchants with same cost for each item
+-- otherwise cost value can be used to determine an enchant when xenchant_random_enchants is enabled
+
 xenchant.register_enchant("fast", "Efficiency", xenchant.help.fast, 1, -0.1, xenchant.action.fast, 0, 1)
 xenchant.register_enchant("fast2", "Efficiency II", xenchant.help.fast, 1, -0.2, xenchant.action.fast, 2, 3)
 
@@ -11,21 +14,21 @@ xenchant.register_enchant("sharp2", "Sharpness II", xenchant.help.sharp, 1, 2, x
 xenchant.register_enchant("durable", "Durability", xenchant.help.durable, 1.5, 0, xenchant.action.durable, 0, 1)
 xenchant.register_enchant("durable2", "Durability II", xenchant.help.durable, 2, 0, xenchant.action.durable, 2, 3)
 
-xenchant.register_enchant("protect", "Protection", xenchant.help.protect, 1.1, 0, xenchant.action.protect, 1, 1)
+xenchant.register_enchant("protect", "Protection", xenchant.help.protect, 1.1, 0, xenchant.action.protect, 0, 1)
 
 xenchant.register_enchant("heal", "Healing", xenchant.help.heal, 1.1, 0, xenchant.action.heal, 1, 3)
 
-xenchant.register_enchant("water", "Water Breathing", xenchant.help.water, 1, 1, xenchant.action.water, 2, 9)
+xenchant.register_enchant("water", "Water Breathing", xenchant.help.water, 1, 1, xenchant.action.water, 2, 15)
 
-xenchant.register_enchant("fire", "Fire Resistance", xenchant.help.fire, 1, 1, xenchant.action.fire, 2, 9)
+xenchant.register_enchant("fire", "Fire Resistance", xenchant.help.fire, 1, 1, xenchant.action.fire, 2, 5)
 
-xenchant.register_enchant("radiation", "Radiation Resistance", xenchant.help.radiation, 1, 1, xenchant.action.radiation, 1, 3)
+xenchant.register_enchant("radiation", "Radiation Resistance", xenchant.help.radiation, 1, 1, xenchant.action.radiation, 1, 5)
 
-xenchant.register_enchant("jump", "Jump Boost", xenchant.help.jump, 0, 0.5, xenchant.action.jump, 1, 3)
-xenchant.register_enchant("jump2", "Jump Boost II", xenchant.help.jump, -0.75, 0.5, xenchant.action.jump, 2, 9)
+xenchant.register_enchant("jump", "Jump Boost", xenchant.help.jump, 0, 0.5, xenchant.action.jump, 1, 5)
+xenchant.register_enchant("jump2", "Jump Boost II", xenchant.help.jump, -0.75, 0.5, xenchant.action.jump, 2, 15)
 
-xenchant.register_enchant("speed", "Speed Boost", xenchant.help.speed, 1, 1, xenchant.action.speed, 1, 3)
-xenchant.register_enchant("speed2", "Speed Boost II", xenchant.help.speed, 1, 2, xenchant.action.speed, 2, 9)
+xenchant.register_enchant("speed", "Speed Boost", xenchant.help.speed, 1, 1, xenchant.action.speed, 1, 5)
+xenchant.register_enchant("speed2", "Speed Boost II", xenchant.help.speed, 1, 2, xenchant.action.speed, 2, 15)
 
 
 
